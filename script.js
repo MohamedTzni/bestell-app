8abab64fd4acb419ca8b0d309b8e714c59d49dba
Mohamed Touzani <mohamed.touzani@gmx.de>
2025-07-28 13:31:00 +0200

    set up DOM element references on window load

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 let cart = [];
 let totalGlobal = 0;
+
+window.onload = function () {
+    const dishesContainer = document.getElementById("dishes-container");
+    const cartContainer = document.getElementsByClassName("cart-content")[0];
+    const orderBtn = document.getElementsByClassName("order-btn")[0];
+    const orderBtnMobile = document.getElementsByClassName("order-btn-mobile")[0];
+    const mobileCartBtn = document.getElementById("mobile-cart-btn");
+    const cartOverlay = document.getElementById("cart-overlay");
+    const closeCart = document.getElementById("close-cart");
+    const cartContentMobile = document.getElementsByClassName("cart-content-mobile")[0];
+
+    const orderPopup = document.getElementById("order-popup");
+    const minPopup = document.getElementById("min-popup");
+    const minPopupText = document.getElementById("min-popup-text");
+    const minContinue = document.getElementById("min-continue");
+    const minCancel = document.getElementById("min-cancel");
+
+    const hamburger = document.getElementsByClassName("hamburger")[0];
+    const navMenu = document.getElementsByClassName("header-right")[0];
